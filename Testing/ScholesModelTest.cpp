607543ffc47725ea409b6538dbf2f9df945dd182
Jack Light <jack.light2023@gmail.com>
2025-02-18 17:39:40 -0500

    Added testing for BlackScholes Constructors.

--- Testing/ScholesModelTest.cpp
+++ Testing/ScholesModelTest.cpp
@@ -1,6 +1,47 @@
 #include "gtest/gtest.h"
+#include "../include/blackScholesModel.h"
 
-TEST(ScholesModelTests, BasicAssert)
+
+TEST(ScholesModelTests, defaultScholesModelConstructorValues)
 {
-    EXPECT_EQ(2 * 3, 6);
+    blackScholesModel b;
+    EXPECT_EQ(b.getUnderlyingPrice(), 0.0);
+
+    EXPECT_EQ(b.getStrikePrice(), 0.0);
+
+    EXPECT_EQ(b.getTimeToExperation(), 0.0);
+
+    EXPECT_EQ(b.getRiskFreeRate(), 0.0);
+
+    EXPECT_EQ(b.getVolatility(), 0.0);
+}
+
+TEST(ScholesModelTests, ScholesModelConstructorWithValuesNoOption)
+{
+    blackScholesModel b(5.6, 4.2, 45.3, 3.14, 2.71);
+    EXPECT_EQ(b.getUnderlyingPrice(), 5.6);
+
+    EXPECT_EQ(b.getStrikePrice(), 4.2);
+
+    EXPECT_EQ(b.getTimeToExperation(), 45.3);
+
+    EXPECT_EQ(b.getRiskFreeRate(), 3.14);
+
+    EXPECT_EQ(b.getVolatility(), 2.71);
+}
+
+TEST(ScholesModelTests, ScholesModelConstructorWithValuesAndOption)
+{
+    blackScholesModel b(5.6, 4.2, 45.3, 3.14, 2.71, blackScholesModel::OptionType::CALL);
+    EXPECT_EQ(b.getUnderlyingPrice(), 5.6);
+
+    EXPECT_EQ(b.getStrikePrice(), 4.2);
+
+    EXPECT_EQ(b.getTimeToExperation(), 45.3);
+
+    EXPECT_EQ(b.getRiskFreeRate(), 3.14);
+
+    EXPECT_EQ(b.getVolatility(), 2.71);
+
+    EXPECT_EQ(b.getOptionType(), blackScholesModel::OptionType::CALL);
 }
