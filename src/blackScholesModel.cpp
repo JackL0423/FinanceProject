e8152a1a29a11da49b2cf36ee69765d3320cd0e9
Jack Light <jack.light2023@gmail.com>
2025-02-23 14:32:13 -0500

    Comments for Black-Scholes functions.

--- src/blackScholesModel.cpp
+++ src/blackScholesModel.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+/// @brief calculates and sets the d1 value in the Black-Scholes model. 
 void blackScholesModel::calculateD1()
 {
     double numerator = log(getUnderlyingPrice()/getStrikePrice()) + (getRiskFreeRate() + 0.5 * getVolatility() * getVolatility()) * getTimeToExperation();
@@ -15,6 +16,7 @@ void blackScholesModel::calculateD1()
     setD1(d1);
 }
 
+/// @brief calculates and sets the d2 value in the Black-Scholes model.
 void blackScholesModel::calculateD2()
 {
     double d2 = getD1() - getVolatility() * sqrt(getTimeToExperation());
@@ -22,7 +24,9 @@ void blackScholesModel::calculateD2()
     setD2(d2);
 }
 
-// May need to double check this equation for the PUT return.
+
+/// @brief calculates the option price using the Black-Scholes model.
+/// @return the option price.
 double blackScholesModel::calculateOptionPrice()
 {
     switch(getOptionType()) 
@@ -38,6 +42,7 @@ double blackScholesModel::calculateOptionPrice()
 }
 
 
+/// @brief calculates and sets the K value in the Black-Scholes model.
 void blackScholesModel::calculateK()
 {
     double K = 1.0 / (1.0 + 0.2316419 * abs(getD1()));
@@ -46,6 +51,7 @@ void blackScholesModel::calculateK()
 }
 
 
+/// @brief default constructor for the Black-Scholes model.
 blackScholesModel::blackScholesModel()
 {
     setUnderlyingPrice(0.0);
@@ -59,6 +65,13 @@ blackScholesModel::blackScholesModel()
     setK(nan(""));
 }
 
+
+/// @brief constructor with no option type for Black-Scholes model.
+/// @param underlyingPrice 
+/// @param strikePrice 
+/// @param timeToExperation 
+/// @param riskFreeRate 
+/// @param volatility 
 blackScholesModel::blackScholesModel(double underlyingPrice, double strikePrice, double timeToExperation,double riskFreeRate, double volatility)
 {
 
@@ -74,6 +87,14 @@ blackScholesModel::blackScholesModel(double underlyingPrice, double strikePrice,
     calculateK();
 }
 
+
+/// @brief constructor with option type for Black-Scholes model.
+/// @param underlyingPrice 
+/// @param strikePrice 
+/// @param timeToExperation 
+/// @param riskFreeRate 
+/// @param volatility 
+/// @param optionType 
 blackScholesModel::blackScholesModel(double underlyingPrice, double strikePrice, double timeToExperation, double riskFreeRate, double volatility, OptionType optionType)
 {
     setUnderlyingPrice(underlyingPrice);
@@ -88,67 +109,103 @@ blackScholesModel::blackScholesModel(double underlyingPrice, double strikePrice,
     calculateK();
 }
 
+/// @brief sets the underlying price in the Black-Scholes model.
+/// @param value 
 void blackScholesModel::setUnderlyingPrice(const double& value) 
 {
     _underlyingPrice = value; 
 }
 
+/// @brief sets the strike price in the Black-Scholes model.
+/// @param value 
 void blackScholesModel::setStrikePrice(const double& value) 
 {
     _strikePrice = value; 
 }
 
+/// @brief sets the time to experation in the Black-Scholes model.
+/// @param value 
 void blackScholesModel::setTimeToExperation(const double& value) 
 {
     _timeToExperation = value; 
 }
 
+/// @brief sets the risk free rate in the Black-Scholes model.
+/// @param value 
 void blackScholesModel::setRiskFreeRate(const double& value) 
 { 
     _riskFreeRate = value; 
 }
 
+/// @brief sets the volatility in the Black-Scholes model.
+/// @param value 
 void blackScholesModel::setVolatility(const double& value) 
 { 
     _volatility = value;
 }
 
+/// @brief sets the option type in the Black-Scholes model.
+/// @param option 
 void blackScholesModel::setOptionType(const OptionType& option) 
 {
     _optionType = option; 
 }
 
+/// @brief sets the D1 value in the Black-Scholes model.
+/// @param value 
 void blackScholesModel::setD1(const double& value) const 
 {
     _d1 = value;
 }
 
+/// @brief sets the D2 value in the Black-Scholes model.
+/// @param value 
 void blackScholesModel::setD2(const double& value) const 
 {
     _d2 = value;
 }
 
+/// @brief sets the K value in the Black-Scholes model.
+/// @param value 
 void blackScholesModel::setK(const double& value) const 
 {
     _K = value;
 }
 
+/// @brief getter for the underlying price in the Black-Scholes model.
+/// @return _underlyingPrice
 const double& blackScholesModel::getUnderlyingPrice() const { return _underlyingPrice; }
 
+/// @brief getter for the strike price in the Black-Scholes model.
+/// @return _strikePrice
 const double& blackScholesModel::getStrikePrice() const { return _strikePrice; }
 
+/// @brief getter for the time to experation in the Black-Scholes model.
+/// @return _timeToExperation
 const double& blackScholesModel::getTimeToExperation() const { return _timeToExperation; }
 
+/// @brief getter for the risk free rate in the Black-Scholes model.
+/// @return _riskFreeRate
 const double& blackScholesModel::getRiskFreeRate() const { return _riskFreeRate; }
 
+/// @brief getter for the volatility in the Black-Scholes model.
+/// @return _volatility
 const double& blackScholesModel::getVolatility() const { return _volatility; }
 
+/// @brief getter for the option type in the Black-Scholes model.
+/// @return _optionType
 const blackScholesModel::OptionType blackScholesModel::getOptionType() const { return _optionType; }
 
+/// @brief getter for the D1 value in the Black-Scholes model.
+/// @return _d1
 const double& blackScholesModel::getD1() const { return _d1; }
 
+/// @brief getter for the D2 value in the Black-Scholes model.
+/// @return _d2
 const double& blackScholesModel::getD2() const { return _d2; }
 
+/// @brief getter for the K value in the Black-Scholes model.
+/// @return _K
 const double& blackScholesModel::getK() const { return _K; }
 
 /// @brief N(x) in Black-Scholes eq.
