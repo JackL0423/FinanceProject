daf74d87e5641636cc1572a95aedcc7f57eb116c
Jack Light <jack.light2023@gmail.com>
2025-02-26 14:42:36 -0500

    testing class added to suite.

---
+++ Testing/test_hestonModel.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+#include "../include/hestonModel.h"
+
+//  TODO: #6 [testing] Write testing suite for hestonModel class
+class hestonModelTest : public testing::Test
+{
+    protected:
+        hestonModelTest()
+        {
+        }
+        // Implement 3 constructors here...
+};
+
