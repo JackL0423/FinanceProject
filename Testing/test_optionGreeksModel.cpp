adc5f498d74dc293d5e6a8e720d4611d82f6a76f
Jack Light <jack.light2023@gmail.com>
2025-02-26 14:42:10 -0500

    Beginning of testing suite for hestonModel.

--- Testing/test_optionGreeksModel.cpp
+++ Testing/test_optionGreeksModel.cpp
@@ -1,3 +1,12 @@
 #include "gtest/gtest.h"
 #include "../include/optionGreeksModel.h"
 
+// TODO: #4 [testing] Write testing suite for optionGreeksModel class
+class optionGreeksModelTest : public testing::Test
+{
+    protected:
+        optionGreeksModelTest()
+        {
+        }
+    // Implement 3 constructors...
+};
