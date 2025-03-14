26e729d2f2bb3e52e76dab9e3a50ef960daaea7e
Jack Light <jack.light2023@gmail.com>
2025-03-13 23:09:03 -0400

    Few more todo's.

--- src/main.cpp
+++ src/main.cpp
@@ -6,3 +6,15 @@ int main()
 
     return 0;
 }
+
+// TODO: Add try-catch statements to avoid crashes when processing data
+//      * Draw out a design and come up with general idea of how best to implement
+//      * Create small program to test design
+//      * Implement
+
+// TODO: Develop more edge case testing for optionGreeks, optionGreeksModel, and BlackScholesModel
+
+// TODO: In-depth review of base-case parameters for model creation
+
+// TODO: Relook at hestonModel::calculateOptionPrice
+//      * lambda functions may be unnecessary, look at using parent functions
