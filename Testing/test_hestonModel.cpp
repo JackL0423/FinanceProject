e901977ffe343fc1178fa373a92486d53793c70e
Jack Light <jack.light2023@gmail.com>
2025-03-16 20:41:23 -0400

    Got rid of To-Do

--- Testing/test_hestonModel.cpp
+++ Testing/test_hestonModel.cpp
@@ -1,7 +1,6 @@
 #include "gtest/gtest.h"
 #include "../include/hestonModel.h"
 
-//  TODO: #7 [testing] Write testing suite for hestonModel class
 class hestonModelTest : public testing::Test
 {
     protected:
