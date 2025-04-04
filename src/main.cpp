420f6864745eb3d707f7d1aae072aabc2a5e99ab
Jack Light <jack.light2023@gmail.com>
2025-04-04 06:05:28 -0400

    small test of model.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
+#include "../include/hestonModel.h"
+#include <random>
 
+using namespace std;
+
+//double newOptionPriceCalc(double, double, double, double, double, double, double, double, double, double, optionGreeksModel::OptionType);
+
+// aiming for ~6.682601659241039
 int main()
 {
-    std::cout<<"Hello World\n";
+    hestonModel model(100.0, 100.0, 1.0, 0.05, 0.2, 0.04, 2.0, 0.04, 0.3, -0.7, optionGreeksModel::OptionType::PUT);
+
+    cout << "QuantLib option price: 6.682601659241039" << endl;
+    cout << "Current model option price: " << model.calculateOptionPrice() << endl;
+    cout << "New model option price: " << model.newCalculateOptionPrice() << endl;
 
     return 0;
 }
-
-// TODO: Add try-catch statements to avoid crashes when processing data
-//      * Draw out a design and come up with general idea of how best to implement
-//      * Create small program to test design
-//      * Implement
 
 // TODO: Develop more edge case testing for optionGreeks, optionGreeksModel, and BlackScholesModel
 
