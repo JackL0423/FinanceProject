d6fe96e25f2ca284f9945e0d3db796f27caf81b2
Jack Light <jack.light2023@gmail.com>
2025-04-04 13:14:06 -0400

    Refactor main function in main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include "../include/hestonModel.h"
-#include <random>
+#include <chrono>
 
 using namespace std;
 
@@ -9,11 +9,28 @@ using namespace std;
 // aiming for ~6.682601659241039
 int main()
 {
-    hestonModel model(100.0, 100.0, 1.0, 0.05, 0.2, 0.04, 2.0, 0.04, 0.3, -0.7, optionGreeksModel::OptionType::PUT);
+    auto start = std::chrono::high_resolution_clock::now();
+
+    double underlyingPrice = 100.0;
+    double strikePrice = 100.0;
+    double timeToExperation = 1.0;
+    double riskFreeRate = 0.05;
+    double volatility = 0.2;
+    double v0 = 0.005;
+    double kappa = 0.8;
+    double theta = 0.04;
+    double sigma = 0.3;
+    double rho = 0.2;
+    optionGreeksModel::OptionType optionType = optionGreeksModel::OptionType::PUT;
+    hestonModel model(underlyingPrice, strikePrice, timeToExperation, riskFreeRate, volatility, v0, kappa, theta, sigma, rho, optionType);
 
     cout << "QuantLib option price: 6.682601659241039" << endl;
-    cout << "Current model option price: " << model.calculateOptionPrice() << endl;
-    cout << "New model option price: " << model.newCalculateOptionPrice() << endl;
+    cout << "model without monte-carlo: " << model.calculateOptionPrice(false, 10000, 1000) << endl;
+    cout << "model with monte-carlo: " << model.calculateOptionPrice(true, 10000, 1000) << endl;
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    cout << "Elapsed time: " << elapsed.count() << " seconds" << endl;
 
     return 0;
 }
