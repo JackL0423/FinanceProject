7fdf1d64863dd7daf24ec39662820b3992247915
Jack Light <jack.light2023@gmail.com>
2025-04-07 03:48:04 -0400

    Comparison of models in main for testing.

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,9 @@ int main()
 {
     auto start = std::chrono::high_resolution_clock::now();
 
-    double underlyingPrice = 100.0;
-    double strikePrice = 100.0;
-    double timeToExperation = 1.0;
+    double underlyingPrice = 50.0;
+    double strikePrice = 45.0;
+    double timeToExperation = 0.0822;
     double riskFreeRate = 0.05;
     double volatility = 0.2;
     double v0 = 0.005;
@@ -21,12 +21,20 @@ int main()
     double theta = 0.04;
     double sigma = 0.3;
     double rho = 0.2;
-    optionGreeksModel::OptionType optionType = optionGreeksModel::OptionType::PUT;
+    optionGreeksModel::OptionType optionType = optionGreeksModel::OptionType::CALL;
     hestonModel model(underlyingPrice, strikePrice, timeToExperation, riskFreeRate, volatility, v0, kappa, theta, sigma, rho, optionType);
 
     cout << "QuantLib option price: 6.682601659241039" << endl;
-    cout << "model without monte-carlo: " << model.calculateOptionPrice(false, 10000, 1000) << endl;
-    cout << "model with monte-carlo: " << model.calculateOptionPrice(true, 10000, 1000) << endl;
+    cout << "Heston model without monte-carlo: " << model.calculateOptionPrice(false, 10000, 1000) << endl;
+    cout << "Heston model with monte-carlo: " << model.calculateOptionPrice(true, 10, 1000) << endl;
+
+    // Test for the black scholes model class
+    blackScholesModel bsModel(underlyingPrice, strikePrice, timeToExperation, riskFreeRate, volatility, optionType);
+    cout << "Black-Scholes model option price: " << bsModel.calculateOptionPrice() << endl;
+
+    // Test for the optionGreeksModel class
+    optionGreeksModel greeksModel(underlyingPrice, strikePrice, timeToExperation, riskFreeRate, volatility);
+    cout << "Option Greeks model option price: " << greeksModel.getOptionPriceIV() << endl;
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
